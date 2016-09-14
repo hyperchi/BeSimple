6af6c9f39b280db6fa4fb1876804052f815c2adb
hyperchi <hyperbolechi@gmail.com>
2016-09-13 23:35:40 -0400

    finish upload tikers for monitoring

--- db/stocks_schema.sql
+++ db/stocks_schema.sql
@@ -5,11 +5,11 @@ CREATE TABLE stocks
 (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     Symbol                         VARCHAR(20),
-    Name                           VARCHAR(30),
+    Name                           VARCHAR(120),
     Exchange                       VARCHAR(20),
     IPOyear                        VARCHAR(20),
-    Sector                         VARCHAR(30),
-    Industry                       VARCHAR(30)
+    Sector                         VARCHAR(50),
+    Industry                       VARCHAR(120)
 );
 
 COMMIT;
---
+++ schedulers/python/populate_data.py
@@ -0,0 +1,53 @@
+import csv
+import MySQLdb
+from databaseQuery import DatabaseQuery
+
+"""
+This file is used for populated stocks data
+one off run
+"""
+
+def open_file(file_name):
+    db = MySQLdb.connect(host="localhost",
+                             user="winstonchi",
+                             db="besimple")
+
+    db_cursor = db.cursor()
+    db.autocommit = True
+
+    attributes = DatabaseQuery.DatabaseQuery.stocks_schema()
+    with open(file_name) as infile:
+        data = csv.reader(infile, delimiter=',')
+        row_count = 0
+        header = []
+        for row in data:
+            if (row_count == 0):
+                header = row
+            else:
+                data_record = {}
+                for index, field in enumerate(header):
+                    if field in attributes:
+                        data_record[field] = row[index]
+                query = DatabaseQuery.DatabaseQuery.get_insert_stocks_query(data_record)
+                try:
+                    db_cursor.execute(query)
+                    db.commit()
+                except MySQLdb.Error, e:
+                    print("something went wrong: {}".format(e))
+                    pass
+            row_count += 1
+    db.close()
+
+def main():
+    """
+    import three main exchange stocks to stocks table
+    :return:
+    """
+    """
+    :return:
+    """
+    file_names = ["nysdaq.csv", "nyse.csv", "amex.csv"]
+    for file_name in file_names:
+        open_file(file_name)
+if __name__ == "__main__":
+    main()
