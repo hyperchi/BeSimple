837067f566fa24b36b578c144d2ee57500800667
hyperchi <hyperbolechi@gmail.com>
2016-09-25 22:08:42 -0400

    rebuild businessLogic

---
+++ web/BusinessLogic/ConnectDB/ConnectDB.py
@@ -0,0 +1,105 @@
+import MySQLdb
+
+class QueryData(object):
+    """
+    This object will be used in Database Query as query objects
+    """
+    def __init__(self, query_string, query_attributes):
+        self.query_string = query_string
+        self.query_attributes = query_attributes
+    def query_string(self):
+        return self.query_string
+    def query_attributes(self):
+        return self.query_attributes
+
+class DatabaseQuery(object):
+    """
+    this is a class containing off database related queries
+    """
+    @staticmethod
+    def stocks_schema():
+        return {
+                  "id"      : "id",
+                  "Symbol"  : "Symbol",
+                  "Name"    : "Name",
+                  "Exchange": "Exchange",
+                  "IPOyear" : "IPOyear",
+                  "Sector"  : "Sector",
+                  "Industry": "Industry"
+                }
+
+    @staticmethod
+    def get_stocks():
+        """
+        get stocks from stocks table
+        @ param void
+        @ return object QueryData
+        """
+
+        stocks_attributes = DatabaseQuery.stocks_schema()
+        query = "SELECT id       as       {id},\
+		                Symbol   as   {Symbol},\
+                        Name     as   {Name},\
+		                Exchange as {Exchange},\
+		                IPOyear  as {IPOyear},\
+		                Sector   as {Sector},\
+                        Industry as {Industry}\
+		                FROM stocks".format(id=stocks_attributes['id'],
+                                            Symbol=stocks_attributes["Symbol"],
+                                            Name=stocks_attributes["Name"],
+                                            Exchange=stocks_attributes["Exchange"],
+                                            IPOyear=stocks_attributes['IPOyear'],
+                                            Sector=stocks_attributes['Sector'],
+                                            Industry=stocks_attributes['Industry'])
+        query_data = QueryData(query, stocks_attributes)
+        return query_data
+
+class ConnectDB(object):
+    def __init__(self):
+        self.host = "localhost"
+        self.username = "winstonchi"
+        self.db_name = "besimple"
+        self.stocks_table_attributes = DatabaseQuery.stocks_schema().keys()
+
+    # getter function to retrieve data from db
+    def basic_getter(self, query):
+        try:
+            db = MySQLdb.connect(host=self.host,
+                                 user=self.username,
+                                 db=self.db_name)
+
+            db_cursor = db.cursor()
+            db_cursor.execute(query)
+            rows = db_cursor.fetchall()
+            db.close()
+            return rows
+        except MySQLdb.Error, e:
+            print("something went wrong: {}".format(e))
+
+   # setter function to retrieve data from db
+    def basic_setter(self, query):
+        try:
+            db = MySQLdb.connect(host=self.host,
+                                 user=self.username,
+                                 db=self.db_name)
+            db.autocommit = True
+            db_cursor = db.cursor()
+            db_cursor.execute(query)
+            db.commit()
+            db.close()
+        except MySQLdb.Error, e:
+            print("something went wrong: {}".format(e))
+
+    def get_stock_info(self, ticker="*"):
+        query = "SELECT * FROM stocks " \
+                "    WHERE Symbol={open_quote}{Symbol}{close_quote}"\
+                     .format(open_quote="'", Symbol=ticker, close_quote="'")
+        print query
+        print self.basic_getter(query)
+        return zip(self.basic_getter(query), self.stocks_table_attributes)
+
+    def get_stock_recent_price(self):
+        pass
+
+    def get_sector_company_list(self):
+        pass
--- web/start.py
+++ web/start.py
@@ -1,34 +1,38 @@
 from flask import Flask, render_template
-from connectDB import ConnectDB
+#from connectDB import ConnectDB
+from BusinessLogic import BusinessLogic
 import logging
 from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
-db_connection = ConnectDB.ConnectDB(app)
+#db_connection = ConnectDB.ConnectDB(app)
 
-get_stocks_query = ConnectDB.DBSchema.get_stocks_query()
-data = db_connection.execute_query(get_stocks_query)
-
-info = data
-for row in data:
-    for elem in row:
-        print elem
+# get_stocks_query = ConnectDB.DBSchema.get_stocks_query()
+# data = db_connection.execute_query(get_stocks_query)
+#
+# info = data
+# for row in data:
+#     for elem in row:
+#         print elem
 @app.route('/')
 
 
 
 def main():
 
-    get_stocks_query = ConnectDB.DBSchema.get_stocks_query()
-    data = db_connection.execute_query(get_stocks_query)
+ #   get_stocks_query = ConnectDB.DBSchema.get_stocks_query()
+  #  data = db_connection.execute_query(get_stocks_query)
 
     # just a test to test if data is actually generated
-    for row in data:
+   # for row in data:
         #app.logger.warning(row)
-        for elem in row:
-            print elem
+    #    for elem in row:
+     #       print elem
+    business_logic = BusinessLogic.BusinessLogic()
+    stock_info = business_logic.get_stock_info_request("AAPL")
     test = "test text"
+    data = stock_info
     return render_template('index.html', info=data, test=test)
 
 if __name__ == "__main__":
